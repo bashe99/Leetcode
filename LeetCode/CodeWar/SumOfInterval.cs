9683cb385d15410a24f61d36fb8a961bb96ce2a7
Bingqian Shen <bashe@microsoft.com>
2019-11-05 16:45:58 +0800

    sum of interval

---
+++ LeetCode/CodeWar/SumOfInterval.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.CodeWar
+{
+    public class Interval
+    {
+        public int min;
+        public int max;
+        public Interval(int min, int max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+    }
+
+    public static class SumOfInterval
+    {
+        private static bool HasOverlap(Interval i1, Interval i2)
+        {
+            if (i1.min == i1.max || i2.min == i2.max)
+            {
+                return false;
+            }
+
+            var overlap = false;
+            if(i2.min < i1.min)
+            {
+                overlap = i2.max > i1.min;
+            }
+            else if (i2.min >= i1.min && i2.min < i1.max)
+            {
+                overlap = true;
+            }
+            else
+            {
+                overlap = false;
+            }
+
+            return overlap;
+        }
+
+        private static void UpdateInterval(List<Interval> iList, Interval i2)
+        {
+            var updateFlag = false;
+            foreach (var i in iList)
+            {
+                if (HasOverlap(i, i2))
+                {
+                    updateFlag = true;
+                    var updatedInterval = new Interval(Math.Min(i.min, i2.min), Math.Max(i.max, i2.max));
+                    i.min = 0;
+                    i.max = 0;
+                    iList.Remove(i);
+                    UpdateInterval(iList, updatedInterval);
+                    break;
+                }
+            }
+
+            if (!updateFlag)
+            {
+                iList.Add(i2);
+            }
+        }
+
+        public static int SumIntervals((int, int)[] intervals)
+        {
+            var iList = new List<Interval>();
+            foreach (var item in intervals)
+            {
+                var tempInterval = new Interval(item.Item1, item.Item2);
+                UpdateInterval(iList, tempInterval);
+            }
+
+            var result = 0;
+            foreach(var item in iList)
+            {
+                result += item.max - item.min;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetcodeTest/CodeWar/SumOfIntervalTests.cs
@@ -0,0 +1,19 @@
+﻿using LeetCode.CodeWar;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeTest.CodeWar
+{
+    [TestClass]
+    public class SumOfIntervalTests
+    {
+        [TestMethod]
+        public void ShouldAddOverlappingIntervals()
+        {
+            var input = new (int, int)[] { (1, 4), (7, 10), (3, 5) };
+            Assert.AreEqual(7, SumOfInterval.SumIntervals(input));
+        }
+    }
+}
