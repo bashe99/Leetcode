217c0ef1d6ed6d71442a2009840fc3839317d618
Bingqian Shen <bashe@microsoft.com>
2019-11-07 13:50:02 +0800

    calculator

---
+++ LeetCode/CodeWar/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.CodeWar
+{
+    public static class Calculator
+    {
+        public static double Evaluate(string expression)
+        {
+            System.Data.DataTable table = new System.Data.DataTable();
+            return Math.Round(Convert.ToDouble(table.Compute(expression, String.Empty)), 6);
+        }
+    }
+}
---
+++ LeetcodeTest/CodeWar/CalculatorTests.cs
@@ -0,0 +1,18 @@
+﻿using LeetCode.CodeWar;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetcodeTest.CodeWar
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void EvaluatorTests()
+        {
+            Assert.AreEqual(9, Calculator.Evaluate("(1+2)*3"));
+        }
+    }
+}
